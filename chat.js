10103cc7d65599fd0f89725b1572dfed5d823f94
Albert Lin <albertlin@dontworyaboutit.dynamic.ucsd.edu>
2016-10-27 14:49:53 -0700

    a

---
+++ chat.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/chat.html');
+});
+
+
+
+io.on('connection',function(socket){
+    socket.on('chat message', function(msg) {
+	//console.log('message: ' + msg);
+	// working one io.emit('chat message',msg);
+	io.emit('chat message',msg);
+	console.log('kjlsahdf;lahsjdfl;kjas');
+    });
+});
+
+http.listen(3000, function(){
+      console.log('listening on *:3000');
+});
