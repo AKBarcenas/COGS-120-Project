fb48d4b0df94bdb68cc0bd713de93172890e60cd
Albert Lin <albertlin@dontworyaboutit.dynamic.ucsd.edu>
2016-10-27 17:21:33 -0700

    new everything

---
+++ app.js
@@ -0,0 +1,73 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars')
+
+var index = require('./routes/index');
+var project = require('./routes/project');
+var preferences = require('./routes/preferences');
+var chat = require('./routes/chat');
+
+
+
+
+
+
+
+// Example route
+// var user = require('./routes/user');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('Intro HCI secret key'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// Add routes here
+app.get('/', index.view);
+app.get('/preferences', preferences.view);
+app.get('/project/:name', project.viewProject);
+// Example route
+// app.get('/users', user.list);
+
+app.get('/chat', chat.view);
+
+
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+
+  var io = require('socket.io').listen(server);
+
+io.on('connection',function(socket){
+    socket.on('chat message', function(msg) {
+	//console.log('message: ' + msg);
+	// working one io.emit('chat message',msg);
+	io.emit('chat message',msg);
+	console.log('kjlsahdf;lahsjdfl;kjas');
+    });
+});
+
+
+  console.log('Express server listening on port ' + app.get('port'));
+});
