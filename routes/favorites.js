a008c5c410e397a1382782206974aee37c4f6a9e
Madeline Hsia <madeline.hsia422@gmail.com>
2016-11-02 00:08:54 -0700

    updeated data.json, reads to favorites

--- routes/favorites.js
+++ routes/favorites.js
@@ -4,19 +4,3 @@ exports.view = function(req, res){
 	console.log(data);
 	res.render('favorites', data);
 };
-/*exports.view	=	function(req,	res){	
-		res.render('favorites',	{	
-			'projects':[
-				{'name':	'Waiting	in	Line',	
-				'image':	'lorempixel.people.1.jpeg',	
-				'id':	'project1'
-			},
-				{'name':	'Needfinding',	
-				'image':	'lorempixel.city.1.jpeg',	
-				'id':	'project2'},
-				{'name':	'	in	Line',	
-				'image':	'lorempixel.people.1.jpeg',	
-				'id':	'project3'},			
-			]	
-		});
-};*/
