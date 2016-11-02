111e336e9db7a0467fec38d8cf533f3d99197531
Madeline Hsia <madeline.hsia422@gmail.com>
2016-11-01 17:55:10 -0700

    prefernce bar, data.json

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-
-
 io.on('connection',function(socket){
     socket.on('chat message', function(msg) {
 	//console.log('message: ' + msg);
