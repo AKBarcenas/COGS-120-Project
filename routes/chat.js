a7b3904ad87c9c8b27d8ea391bc4c9969ae5ed9e
AKBarcenas <akbarcen@ucsd.edu>
2016-11-06 12:11:01 -0800

    Adding more chat options for the bot.

--- routes/chat.js
+++ routes/chat.js
@@ -1,3 +1,8 @@
+var data = require('../data.json');
+var fs = require('fs');
+
 exports.view = function(req, res){
 	res.render('chat');	
+	//res.render('favorites', data);
+	//res.render('fs');
 };
