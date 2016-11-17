0b359e731c414264a540cc3c53f59749d877174f
AKBarcenas <akbarcen@ucsd.edu>
2016-11-16 21:45:47 -0800

    Get rid of commented out dead code.

--- routes/foodPlace.js
+++ routes/foodPlace.js
@@ -21,30 +21,4 @@ exports.getFoodPlace = function(req, res) {
 			"placeAddress": ""
 		});
     }
-	
-
-
-	/*console.log(J);
-	console.log(C);
-	console.log("dasfdsfdsjfdsfdjskfd");
-	console.log(counter);*/
-
-	/*var name = req.query.name;
-	var address = req.query.address;
-	var food = req.query.food;
-	console.log("AYY MACARENA");
-	//console.log(req);
-	console.log(name);
-	console.log(address);
-	console.log(food);
-
-	var newFavorite =
-	{
-      "name": name,
-      "description": food,
-      "location": address
-    }
-
-    data.favorites.push(newFavorite);*/
-    
 }
