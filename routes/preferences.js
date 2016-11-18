1dfe022a24c5d7f2c22a07181775b5f44d27907d
Albert Lin <albertxp@gmail.com>
2016-11-17 20:02:02 -0800

    Fixed A/B testing

--- routes/preferences.js
+++ routes/preferences.js
@@ -1,8 +1,11 @@
 exports.view = function(req, res){
-  
-  res.render('preferences', {'showAlternate': 'false'});
+  showAlternate = false;
+  res.render('preferences', showAlternate);
+  //res.render('preferences', {'showAlternate': 'false'});
 };
 
 exports.viewAlt = function(req, res){
+  showAlternate = true;
+  //res.render('preferences', showAlternate);
   res.render('preferences', {'showAlternate': 'true'});
 }
