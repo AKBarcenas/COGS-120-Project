3a82c631e8a9581b1128a5557123f0351be589f5
AKBarcenas <akbarcen@ucsd.edu>
2017-04-23 22:54:33 -0700

    Change the app.js file for Yelp Fusion

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ var about = require('./routes/about');
 
 var foodPlace = require('./routes/foodPlace');
 var set = require('./routes/set');
+var yelp = require('./routes/yelp');
 
 // Example route
 // var user = require('./routes/user');
@@ -67,6 +68,7 @@ app.get('/get', set.getPreference);
 
 app.get('/newchat', newchat.view);
 app.get('/about', about.view);
+app.get('/yelp', yelp.getRestaurants);
 
 
 var server = http.createServer(app).listen(app.get('port'), function(){
